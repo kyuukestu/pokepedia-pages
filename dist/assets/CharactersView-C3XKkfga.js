0d90f1b61843e1a875dd6c77ec3ce84ae328b6d5
kyuukestu <kyuukestu@gmail.com>
2025-06-16 20:31:11 -0500

    removed superfluous file

--- dist/assets/CharactersView-C3XKkfga.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,s as r,o as c}from"./index-DVqrgAlk.js";const a={};function t(s,n){return c(),r("h1",null,"Characters Page")}const _=e(a,[["render",t]]);export{_ as default};
---
+++ dist/assets/SwarmsView-CUPGS-fr.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as r}from"./index-DoGJt-YA.js";const n={};function o(s,t){return r(),c("h1",null,"Swarms")}const _=e(n,[["render",o]]);export{_ as default};
