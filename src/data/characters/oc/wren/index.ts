52a157bbd2847c6e7d0b6a876e5d0970bb6ee195
J. A-L G. P. <kyuukestu@gmail.com>
2026-04-17 04:51:18 -0500

    added Wren

---
+++ src/data/characters/oc/wren/index.ts
@@ -0,0 +1,18 @@
+import { CharacterMeta } from '@/types/character'
+
+const meta: CharacterMeta = {
+  id: 'Wren',
+  category: 'oc',
+  name: {
+    full: 'Wren Blackwell',
+    short: [],
+  },
+  badges: 3,
+  ribbons: 0,
+  color: '',
+  region: 'sinnoh',
+  gender: 'female',
+  trainerClass: ['battler'],
+}
+
+export default meta
