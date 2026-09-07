5a78b4394434c971152c89efda1cf177c4a063ac
J. A-L G. P. <kyuukestu@gmail.com>
2026-09-07 00:38:15 -0500

    Added categories

---
+++ src/data/events/categories/conference.ts
@@ -0,0 +1,6 @@
+import type { EventCategory } from "@/types/events";
+
+export default {
+  id: "conference",
+  name: "Regional Conference", 
+} satisfies EventCategory;
---
+++ src/data/events/categories/other.ts
@@ -0,0 +1,6 @@
+import type { EventCategory } from "@/types/events";
+
+export default {
+  id: "other",
+  name: "Other", 
+} satisfies EventCategory;
